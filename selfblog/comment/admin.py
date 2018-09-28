4fed47d6cefe6cfe6fcfeb9d2d18da74c4885107
jummy <929440925@qq.com>
2018-09-28 17:48:34 +0800

    添加一级评论功能

--- selfblog/comment/admin.py
+++ selfblog/comment/admin.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import PostComment
+from selfblog.custom_site import custom_site
+from selfblog.custom_admin import BaseOwnerAdmin
+
+
+@admin.register(PostComment, site=custom_site)
+class PostAdmin(BaseOwnerAdmin):
+    list_display =['user', 'content']
--- selfblog/comment/views.py
+++ selfblog/comment/views.py
@@ -2,8 +2,7 @@
 from __future__ import unicode_literals
 
 from django.views.generic import TemplateView
-from django.urls import reverse, NoReverseMatch
-from django.core.exceptions import ImproperlyConfigured
+from django.shortcuts import redirect
 
 from .models import PostComment
 from .forms import PostCommentForm
@@ -16,10 +15,8 @@ class CommentShowMixin(object):
         return comments
 
     def get_context_data(self, **kwargs):
-        content = PostCommentForm()
-        content.fields['article'].widget.attrs['value']=self.kwargs['pk']
         kwargs.update({
-            'comment_form': content,
+            'comment_form': PostCommentForm(initial={'article': self.kwargs['pk']}),
             'comment_list': self.get_comments(),
         })
         return super(CommentShowMixin, self).get_context_data(**kwargs)
@@ -28,3 +25,25 @@ class CommentShowMixin(object):
 class CommentView(TemplateView):
     http_method_names = ['post']
     template_name = 'comment/post_comment.html'
+
+    def post(self, request, *args, **kwargs):
+        comment_form = PostCommentForm(request.POST)
+        article = request.POST.get('article')
+        post = request.POST.get('post')
+
+        if comment_form.is_valid():
+            instance=comment_form.save(commit=False)
+            instance.article.pk = article
+            instance.user_id = request.user.id
+            instance.save()
+            succeed = True
+            print(type(post))
+            return redirect(post)
+        else:
+            succeed = False
+        context = {
+            'succeed': succeed,
+            'form': comment_form,
+            'article': article,
+        }
+        return self.render_to_response(context)
